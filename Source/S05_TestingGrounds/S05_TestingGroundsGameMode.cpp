72c356b651f4de4f524d4c9b710616e2896b0319
coatsandhats <lucascoats663@gmail.com>
2018-08-16 00:26:47 -0400

    TG 17

--- Source/S05_TestingGrounds/S05_TestingGroundsGameMode.cpp
+++ Source/S05_TestingGrounds/S05_TestingGroundsGameMode.cpp
@@ -2,7 +2,7 @@
 
 #include "S05_TestingGroundsGameMode.h"
 #include "S05_TestingGroundsHUD.h"
-#include "Player/FirstPersonCharacter.h"
+#include "S05_TestingGroundsCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AS05_TestingGroundsGameMode::AS05_TestingGroundsGameMode()
